36a126161afe6f1b85fa3056a0bfcccef6abe702
dougretrogames <doug_fukuoka10@hotmail.com>
2026-07-24 10:17:44 -0300

    Feat: Adiciona diálogo de instruções de uso e traduções atualizadas

--- app/src/main/java/com/dougretrogames/gamepadklfixer/ui/MainActivity.kt
+++ app/src/main/java/com/dougretrogames/gamepadklfixer/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -43,6 +44,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.refresh()
+        showInstructionsIfNeeded()
+    }
+
+    private fun showInstructionsIfNeeded() {
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        val showInstructions = prefs.getBoolean("show_instructions", true)
+        
+        if (showInstructions) {
+            val content = getString(R.string.instructions_content)
+            val styledContent = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                android.text.Html.fromHtml(content, android.text.Html.FROM_HTML_MODE_COMPACT)
+            } else {
+                @Suppress("DEPRECATION")
+                android.text.Html.fromHtml(content)
+            }
+
+            AlertDialog.Builder(this)
+                .setTitle(R.string.instructions_title)
+                .setMessage(styledContent)
+                .setPositiveButton(R.string.instructions_button) { _, _ ->
+                    prefs.edit().putBoolean("show_instructions", false).apply()
+                }
+                .setCancelable(false)
+                .show()
+        }
     }
 
     override fun onResume() {
